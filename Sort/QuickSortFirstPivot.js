629ae7c507d858a74cdf2a16f7be637492434ea4
Naren <naren.sharmendran@gmail.com>
2019-08-12 15:59:40 +0530

    Added quick sort with first pivot

--- Sort/QuickSortFirstPivot.js
+++ Sort/QuickSortFirstPivot.js
@@ -1,7 +1,7 @@
 /**
  * Geeks: https://www.geeksforgeeks.org/quick-sort/
  */
-class QuickSortLastPivot {
+class QuickSorFirstPivot {
 	constructor(arr) {
 		this.arr = arr;
 	}
@@ -23,32 +23,31 @@ class QuickSortLastPivot {
 	/*Pivot is implemented based upon middle*/
 	sortPartitionByLastPivot(arr, startIndex, endIndex) {
 
-		let pivotValue = arr[endIndex];
+		let pivotValue = arr[startIndex];
 
-		let lowItr = startIndex - 1;
+		let highItr = endIndex + 1;
 
 		/* Iterate till left itr keeps smaller than right itr */
-		for(let i = startIndex; i <= (endIndex - 1); ++i) {
+		for(let i = endIndex; i > startIndex; --i) {
+			if(arr[i] >= pivotValue) {
+                highItr--;
 
-			if(arr[i] <= pivotValue) {
-                lowItr++;
-
-				/* Swap lowItr and i.*/
-				let temp = arr[lowItr];
-				arr[lowItr] = arr[i];
+				/* Swap highItr and i.*/
+				let temp = arr[highItr];
+				arr[highItr] = arr[i];
 				arr[i] = temp;
 			}
 		}
 
-        /* Swap lowItr+1 and endIndex */
-        lowItr++;
-		let temp = arr[lowItr];
-		arr[lowItr] = arr[endIndex];
-		arr[endIndex] = temp;
+        /* Swap highItr+1 and endIndex */
+        highItr--;
+		let temp = arr[highItr];
+		arr[highItr] = arr[startIndex];
+		arr[startIndex] = temp;
 
-		return lowItr;
+		return highItr;
 	}
 }
 
-qs = new QuickSortLastPivot([1, 4, 5, 7, 8, 2, 1, 5, 6, 8, 0, -10, 8, 3, 9, -1, 0]);
+qs = new QuickSorFirstPivot([1, 4, 5, 7, 8, 2, 1, 5, 6, 8, 0, -10, 8, 3, 9, -1, 0]);
 qs.implementQuickSort();
