019011704ecf2b888b1d36ad4be0d28d75aa074b
Naren <naren.sharmendran@gmail.com>
2019-08-12 18:17:31 +0530

    Added coding comments

--- Number/HappyNumber.js
+++ Number/HappyNumber.js
@@ -1,17 +1,22 @@
 /**
+ * Returns square sum of a number.
+ * For 23, it will return (2*2) + (3*3) => 13
  * 
  * @param {Number} inputNumber 
  */
-
 function getSquareSum(input) {
     let squareSum = 0;
     while(input !== 0) {
-        squareSum += ((input %10) * (input %2));
+        remainderNumber = (input % 10)
+        squareSum += (remainderNumber * remainderNumber);
         input = Math.floor(input/10);
     }
     return squareSum;
 }
 
+/**
+ * Validates whether its a happy number or not.
+ */
 function isHappyNumber (inputNumber) {
     let visitedSet = new Set();
     while(!visitedSet.has(inputNumber)) {
